4b502eb24b265f157aee922e9a7f6380ef27c969
Ammar Askar <ammar_askar@hotmail.com>
2012-03-03 14:31:16 +0500

    Broadcast /me on irc

--- src/to/joe/j2mc/chat/command/MeCommand.java
+++ src/to/joe/j2mc/chat/command/MeCommand.java
@@ -1,5 +1,7 @@
 package to.joe.j2mc.chat.command;
 
+import java.util.HashSet;
+
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
@@ -7,11 +9,15 @@ import to.joe.j2mc.chat.J2MC_Chat;
 import to.joe.j2mc.core.J2MC_Core;
 import to.joe.j2mc.core.J2MC_Manager;
 import to.joe.j2mc.core.command.MasterCommand;
+import to.joe.j2mc.core.event.MessageEvent;
 
 public class MeCommand extends MasterCommand {
 
+    J2MC_Chat plugin;
+    
     public MeCommand(J2MC_Chat j2mc_chat) {
         super(j2mc_chat);
+        this.plugin = j2mc_chat;
     }
 
     @Override
@@ -23,6 +29,9 @@ public class MeCommand extends MasterCommand {
                     pl.sendMessage("*" + player.getDisplayName() + " " + message);
                 }
             }
+            HashSet<String> targets = new HashSet<String>();
+            targets.add("GAMEMSG");
+            plugin.getServer().getPluginManager().callEvent(new MessageEvent(targets, "*" + player.getName() + " " + message));
         }
     }
 
