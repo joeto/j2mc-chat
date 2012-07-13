f8521c1fda999f52ab0714e6a424aee1b892fb61
Ivan Panarusky <eviltechie@joe.to>
2012-07-12 23:39:22 -0400

    the big half of muteall

---
+++ src/to/joe/j2mc/chat/command/MuteallCommand.java
@@ -0,0 +1,32 @@
+package to.joe.j2mc.chat.command;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import to.joe.j2mc.chat.J2MC_Chat;
+import to.joe.j2mc.core.J2MC_Manager;
+import to.joe.j2mc.core.command.MasterCommand;
+
+public class MuteallCommand extends MasterCommand {
+	
+	J2MC_Chat plugin;
+
+    public MuteallCommand(J2MC_Chat chat) {
+        super(chat);
+        this.plugin = chat;
+    }
+    
+    @Override
+    public void exec(CommandSender sender, String commandName, String[] args, Player player, boolean isPlayer) {
+    	this.plugin.everbodyMuted = !this.plugin.everbodyMuted;
+    	if (this.plugin.everbodyMuted) {
+    		J2MC_Manager.getCore().adminAndLog(ChatColor.RED + sender.getName() + " has muted all players");
+    		this.plugin.getServer().broadcastMessage(ChatColor.RED + "All players have been muted. You may still use /msg");
+    	} else {
+    		J2MC_Manager.getCore().adminAndLog(ChatColor.RED + sender.getName() + " has unmuted all players");
+    		this.plugin.getServer().broadcastMessage(ChatColor.RED + "All players have been unmuted");
+    	}
+    }
+
+}
